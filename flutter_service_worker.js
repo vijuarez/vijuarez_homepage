156ba2fc716e30882f934e29b453bc276a6a7455
Vicente Juárez <vijuarez@uc.cl>
2022-05-12 05:32:47 -0400

    Built web
    Branch: main
    Commit: 6a0fbfc5ea259d2b4365500947512071a73f5468
    
    package:peanut 4.1.1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "c8658431bfe3ecd888260eb47f17d596",
-"/": "c8658431bfe3ecd888260eb47f17d596",
+  "index.html": "2a25e3a83e04289a1dcbe6b0083d1c3f",
+"/": "2a25e3a83e04289a1dcbe6b0083d1c3f",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
